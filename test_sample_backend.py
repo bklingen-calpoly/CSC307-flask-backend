2283c882ea0f56555f956f80a528c9b78b0b5622
BJ Klingenberg <bklingen@calpoly.edu>
2021-11-03 12:42:32 -0700

    fix build

--- test_sample_backend.py
+++ test_sample_backend.py
@@ -5,7 +5,7 @@ def test_find_users_by_name_success():
     expected = [           
         {
            '_id' : '6009dfa508f2eca30c87d56c',            
-            'name': 'Macc',
+            'name': 'Mac',
             'job': 'Professor',
         },   
         {
